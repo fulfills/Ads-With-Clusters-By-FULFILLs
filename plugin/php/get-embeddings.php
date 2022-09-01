f5b508b6c97c9c70e48002bbf82da09f211cb4fe
Kai Sugahara <git@ksugahara.com>
2022-09-01 23:16:19 +0900

    Clean:

--- plugin/php/get-embeddings.php
+++ plugin/php/get-embeddings.php
@@ -4,53 +4,43 @@
         check_ajax_referer('awcbf_embeddings_ajax','secure');
 		header('Content-type: text/plain; charset= UTF-8');
 
-        // switch()
-        if(awcbf_sanitize_for_array($_REQUEST['target']) === 'all') {
-
-            $args = array(
-                'posts_per_page'   => -1,
-                'post_type'        => 'post',
-                'post_status'      => 'all',
-                'suppress_filters' => true 
-            );
-
-        }
-        elseif(awcbf_sanitize_for_array($_REQUEST['target']) === 'only_publish') {
-
-            $args = array(
-                'posts_per_page'   => -1,
-                'post_type'        => 'post',
-                'post_status'      => 'publish',
-                'suppress_filters' => true 
-            );
-
-        }
-
-        $posts_array = get_posts( $args );
-        $TITLEs = array_column($posts_array, 'post_title', 'ID');
-
-        $url = 'https://corpus.fulfills.jp';
-        $data = [
-            'data' => $TITLEs
-        ];
-        $context = array(
-            'http' => array(
-                'method'  => 'POST',
-                'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
-                'content' => http_build_query($data)
-            )
+        // GET WP-POST(s)
+        $args = array(
+            'posts_per_page'   => -1,
+            'post_type'        => 'post',
+            'post_status'      => ((awcbf_sanitize_for_array($_REQUEST['target']) === 'only_publish') ? 'publish' : 'all'),
+            'suppress_filters' => true 
         );
-        $html = file_get_contents($url, false, stream_context_create($context));
-        $output = json_decode($html, 1);
+        $posts = get_posts( $args );
+
+        // GENERATE TITLE-DICT {ID: POST_TITLE}
+        $TITLEs = json_encode(array_column($posts, 'post_title', 'ID'));
+
+        // WebAPI URL
+        $url = 'https://corpus.fulfills.jp';
+
+        // CHANGE TITLE-DICT to CONTEXT on POST-REQUEST
+        $options = array(
+				'http' => array(
+					'method'=> 'POST',
+					'header'=> 'Content-type: application/json; charset=UTF-8',
+					'content' => $TITLEs
+				)
+			);
+		$context = stream_context_create($options);
+		
+        // POST-REQUEST & DECODE RESPONSE TO JSON
+        $OUTPUT = file_get_contents($url, FALSE, $context);
+        $OUTPUT = json_decode($OUTPUT, 1);
 
         // ERROR CHECK
-        if(!$output['flag']) {
-            echo esc_html($output['message']);
+        if(!$OUTPUT['flag']) {
+            echo esc_html($OUTPUT['message']);
             wp_die();
         }
-
+		
         // SAVE
-        update_option( 'awcbf_embeddings', $output['vectors'] );
+        update_option( 'awcbf_embeddings', $OUTPUT['vectors'] );
 
         wp_die();
 
